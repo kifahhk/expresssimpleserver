9a7dbc2fa719dae27b0f1bdd161485de6116ca7e
Kifah Seif <kifahhk@yahoo.com>
2016-09-03 15:26:19 +0200

    =add pages template using jade

--- app.js
+++ app.js
@@ -5,10 +5,16 @@ const nodemailer = require('nodemailer');
 
 var app = express();
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.get('/', (req, res) => res.send('Hello World'));
+app.get('/', (req, res) => res.render('index', {title: 'Home'}));
+app.get('/about', (req, res) => res.render('about', {title: 'About'}));
+app.get('/contact', (req, res) => res.render('contact', {title: 'Contact'}));
+
 
 app.listen(2200);
 console.log('Server is running on Port', 2200);
