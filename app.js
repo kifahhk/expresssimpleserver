310ec60f5bb275ec0acde5262dc16dad1c57f3e3
Kifah Seif <kifahhk@yahoo.com>
2016-09-03 15:47:41 +0200

    =add contact form using nodmailer

--- app.js
+++ app.js
@@ -14,6 +14,33 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.get('/', (req, res) => res.render('index', {title: 'Home'}));
 app.get('/about', (req, res) => res.render('about', {title: 'About'}));
 app.get('/contact', (req, res) => res.render('contact', {title: 'Contact'}));
+app.post('/contact/send', (req, res) => {
+	var transporter = nodemailer.createTransport({
+		service: 'Gmail',
+		auth: {
+			user: 'user',
+			pass: 'password'
+		}
+	});
+
+	var mailOptions = {
+		from: 'Kifah Seif <kifahhk@gmail.com>',
+		to: 'kifahhk@yahoo.com',
+		subject: 'Contact',
+		text: 'Name: '+req.body.name+'Email: '+req.body.email+ 'Message: '+req.body.message,
+		html: '<ul><li>Name: '+req.body.name+'</li><li>Email: '+req.body.email+'</li><li>Message: '+req.body.message+'</li></ul>'
+	};
+
+	transporter.sendMail(mailOptions, function(error, info){
+		if(error){
+			console.log(error);
+			res.redirect('/');
+		} else {
+			console.log('Message Sent: '+info.response);
+			res.redirect('/');
+		}
+	});
+});
 
 
 app.listen(2200);
